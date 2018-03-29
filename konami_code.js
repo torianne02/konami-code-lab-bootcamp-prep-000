7c5b26fced7d03f3807af911b76df355862a2096
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-29 13:39:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  
+  // Keep track of index outside of the event handler.
+  let index = 0;
+  console.log('test')
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+    console.log(e)
+    console.log(e.detail)
+    console.log(e.which)
+    const key = parseInt(e.detail || e.which);
+   
+    if (key === code[index]) {
+      index++;
+   
+      if (index === code.length) {
+        alert("Hurray!");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 
 }
